b74cce104a851e9ce17e3e2099509438c9b12af9
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-10-26 15:17:47 +0530

    Initial code

---
+++ src/main/java/com/Backend/PACER/PacerApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.PACER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Backend/PACER/PacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.PACER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
