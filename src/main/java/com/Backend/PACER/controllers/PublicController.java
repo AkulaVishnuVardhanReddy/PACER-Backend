edf54afd03c0a45bc0407d376957eca0198f1965
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-11-04 16:09:30 +0530

    Login History Success

--- src/main/java/com/Backend/PACER/controllers/PublicController.java
+++ src/main/java/com/Backend/PACER/controllers/PublicController.java
@@ -2,13 +2,16 @@ package com.Backend.PACER.controllers;
 
 import com.Backend.PACER.entities.CaseAccessHistory;
 import com.Backend.PACER.entities.LoginHistory;
+import com.Backend.PACER.entities.User;
 import com.Backend.PACER.services.CaseAccessHistoryService;
 import com.Backend.PACER.services.LoginHistoryService;
+import com.Backend.PACER.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class PublicController {
@@ -19,6 +22,9 @@ public class PublicController {
     @Autowired
     private CaseAccessHistoryService caseAccessHistoryService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/login-history")
     public LoginHistory createLoginHistory(@RequestBody LoginHistory loginHistory) {
         return loginHistoryService.createLoginHistory(loginHistory);
@@ -27,6 +33,12 @@ public class PublicController {
     @PostMapping("/case-history")
     public CaseAccessHistory createHistory(@RequestBody CaseAccessHistory history) {
         return caseAccessHistoryService.createCaseAccessHistory(history);
+    }
+
+    @GetMapping("/findUserId/{username}")
+    public ResponseEntity<Long> findUserIdByUserName(@PathVariable String username) {
+        Optional<User> user = userService.findByUsername(username);
+        return ResponseEntity.ok(user.get().getId());
     }
 
 }
